260640510671e1379d805ac570e0f2ff33f87995
dhondta <alexandre.dhondt@gmail.com>
2019-11-22 16:37:22 +0100

    Improved framework launcher script

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 #!/usr/bin/python3
-import os
 from sploitkit import FrameworkConsole
+from sploitkit.utils.dict import ExpiringDict
+from tinyscript import *
 
 
 class DronesploitConsole(FrameworkConsole):
     sources = {
-        'banners': "./banners",
+        'banners':   "./banners",
+        'libraries': "./lib",
     }
     exclude = ["root/test", "root/help"]
+    
+    def __init__(self, *args, **kwargs):
+        self.interfaces
+        self.state['TARGETS'] = ExpiringDict(max_age=300)
+        super(DronesploitConsole, self).__init__(*args, **kwargs)
+
+    @property
+    def interfaces(self):
+        d = self.state['INTERFACES'] = {}
+        out = self._jobs.run("iwconfig", no_debug=True)[0]
+        ifaces = re.split(r"\n\s*\n", out)
+        for i in ifaces:
+            if "no wireless extensions" not in i:
+                d[i.split()[0]] = "Mode:Monitor" in i
+        return d.keys()
+    
+    @property
+    def mon_interfaces(self):
+        return [i for i, mon in self.state['INTERFACES'].items() if mon]
 
 
 if __name__ == '__main__':
-    if os.geteuid() != 0:
-        print("This should be run as sudo")
-    else:
-        DronesploitConsole(
-            "DroneSploit",
-            banner_section_styles={'title': {'fgcolor': "random"}},
-        ).start()
+    parser.add_argument("--dev", action="store_true", help="development mode")
+    initialize(globals(), sudo=True)
+    DronesploitConsole(
+        "DroneSploit",
+        banner_section_styles={'title': {'fgcolor': "random"}},
+        dev=args.dev,
+    ).start()
