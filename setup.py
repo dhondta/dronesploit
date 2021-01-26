42029194aa974cc471531a1c4ee568c66c3115d5
dhondta <alexandre.dhondt@gmail.com>
2021-01-26 12:01:00 +0100

    Updated requirements + New release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 data = {'': ["VERSION.txt"]}
 for folder in ["banners", "commands", "modules"]:
-    for root, _, files in os.walk(os.path.join("dronesploit", folder)):
+    for root, _, files in os.walk(os.path.join("dronesploit", "_src", folder)):
         _, root = root.split(os.path.sep, 1)
         for f in files:
             data[''].append(os.path.join(root, f))
